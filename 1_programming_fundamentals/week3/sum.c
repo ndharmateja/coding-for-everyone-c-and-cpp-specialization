c5f54e10f3350ed040e8f4292821e9cdf7a4ae3d
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-11-21 19:20:21 +0530

    Add sum.c

---
+++ 1_programming_fundamentals/week3/sum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int sum = 0, n = 10;
+
+    for (int i = 1; i <= n; i++)
+    {
+        sum += i;
+    }
+
+    printf("Sum of first %d natural numbers: %d\n\n", n, sum);
+}
