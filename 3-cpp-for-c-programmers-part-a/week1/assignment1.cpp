5b92e4f746e6f611cee50b37d94f0f77cd129ccf
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-11-15 02:50:01 +0530

    add comments

--- 3-cpp-for-c-programmers-part-a/week1/assignment1.cpp
+++ 3-cpp-for-c-programmers-part-a/week1/assignment1.cpp
@@ -9,9 +9,11 @@ using namespace std;
 const int n = 40;
 
 // Generic sum function
+// The first and the 3rd arguments are passed by reference
 template <class Summable>
-inline void sum(Summable &p, int size, vector<Summable> d)
+inline void sum(Summable &p, int size, vector<Summable> &d)
 {
+    // Loop over the vector and accumulate the sum in the passed value
     p = 0;
     for (int i = 0; i < size; ++i)
     {
@@ -23,14 +25,17 @@ inline void sum(Summable &p, int size, vector<Summable> d)
 int main()
 {
     // Variable declaration
+    // Variable to store the sum and the vector
     int accum = 0;
     vector<int> data(n);
 
-    // Compute the sum
+    // Initialize the vector of 'n' values
     for (int i = 0; i < n; ++i)
     {
         data[i] = i;
     }
+
+    // Compute the sum and store the result in the 'accum' variable
     sum(accum, n, data);
 
     // Output the results
