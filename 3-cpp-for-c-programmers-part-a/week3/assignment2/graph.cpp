a795f70ed064bc4c0d46a8676fda06b8c6d87896
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-11-19 15:15:03 +0530

    change graph interface

--- 3-cpp-for-c-programmers-part-a/week3/assignment2/graph.cpp
+++ 3-cpp-for-c-programmers-part-a/week3/assignment2/graph.cpp
@@ -12,21 +12,48 @@ using namespace std;
 class Graph
 {
 private:
-    vector<vector<WeightedEdge>> list;
-
-public:
-    Graph(int n)
+    vector<vector<double>> list;
+    void initialize_list(int n)
     {
         for (int i = 0; i < n; i++)
         {
-            vector<WeightedEdge> neighbours;
+            vector<double> neighbours(n);
+            for (int j = 0; j < n; j++)
+                neighbours[j] = 0.0;
+
             this->list.push_back(neighbours);
         }
     }
 
-    vector<WeightedEdge> get_neighbours(int v) const { return list[v]; }
+public:
+    Graph(int n) { initialize_list(n); }
+    Graph(const Graph &g) { this->list = vector<vector<double>>(g.list); }
+
     int get_num_vertices() const { return this->list.size(); }
-    void add_edge(int from, int to, double weight) { this->list[from].push_back(WeightedEdge(from, to, weight)); }
+    int get_num_edges()
+    {
+        int edges = 0;
+        for (int i = 0; i < list.size(); i++)
+            for (int j = 0; j < list.size(); j++)
+                if (list[i][j] > 0.0)
+                    edges++;
+        return edges;
+    }
+
+    bool is_adjacent(int u, int v) const { return list[u][v] > 0.0; }
+    vector<int> get_neighbours(int u) const
+    {
+        vector<int> neighbours;
+        for (int i = 0; i < list.size(); i++)
+            if (is_adjacent(u, i))
+                neighbours.push_back(i);
+        return neighbours;
+    }
+
+    void add_edge(int u, int v, double weight) { this->list[u][v] = weight; }
+    void delete_edge(int u, int v) { this->list[u][v] = 0.0; }
+
+    double get_edge_weight(int u, int v) const { return this->list[u][v]; }
 };
 
 #endif
