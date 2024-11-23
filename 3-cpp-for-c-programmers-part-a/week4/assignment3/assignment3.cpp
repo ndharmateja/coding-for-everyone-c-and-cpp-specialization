c1bb5cdf6d5103b379bca036527325bda2696aa0
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-11-23 16:56:15 +0530

    create graph with constructor using a file

--- 3-cpp-for-c-programmers-part-a/week4/assignment3/assignment3.cpp
+++ 3-cpp-for-c-programmers-part-a/week4/assignment3/assignment3.cpp
@@ -8,25 +8,16 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     // Create graph and add edges
-    int num_vertices = 4;
-    Graph g(num_vertices);
-    g.add_edge(0, 1, 1);
-    g.add_edge(0, 2, 7);
-    g.add_edge(1, 0, 1);
-    g.add_edge(1, 3, 2);
-    g.add_edge(2, 0, 7);
-    g.add_edge(2, 3, 3);
-    g.add_edge(3, 1, 2);
-    g.add_edge(3, 2, 3);
+    Graph g("sample_data1.txt");
 
     // Print the graph
-    cout << "Graphhh: " << endl;
+    cout << "Graph: " << endl;
     cout << g << endl;
 
     // Run Prim's and print the MST
     Prims prims(g);
-    cout << "MST cost: " << prims.get_mst_cost() << endl;
     prims.print_mst();
+    cout << "Cost: " << prims.get_mst_cost() << endl;
 
     return 0;
 }
