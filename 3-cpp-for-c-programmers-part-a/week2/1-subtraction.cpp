ca66a304dd421ceaf658847d146fcf82854d4d74
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-05-22 17:22:57 +0530

    rename display function to subtract

--- 3-cpp-for-c-programmers-part-a/week2/1-subtraction.cpp
+++ 3-cpp-for-c-programmers-part-a/week2/1-subtraction.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 template <class T>
-T display(const T data[], int size, T init = 0)
+T subtract(const T data[], int size, T init = 0)
 {
     for (int i = 0; i < size; i++)
     {
@@ -16,17 +16,15 @@ T display(const T data[], int size, T init = 0)
 int main(int argc, char const *argv[])
 {
     int a[] = {1, 2, 3};
-    cout << display(a, 3) << endl;
-    cout << display(a, 3, 10) << endl;
+    cout << subtract(a, 3) << endl;
+    cout << subtract(a, 3, 10) << endl;
 
     double b[] = {-1, 2.5, 3.75};
-    cout << display(b, 3) << endl;
-    cout << display(b, 3, 10.0) << endl;
+    cout << subtract(b, 3) << endl;
+    cout << subtract(b, 3, 10.0) << endl;
 
-    complex<double> c[] = {
-        complex<double>(2.5, 3.75),
-        complex<double>(-2.5, 13.75)};
-    cout << display(c, 2) << endl;
+    complex<double> c[] = {complex<double>(2.5, 3.75), complex<double>(-2.5, 13.75)};
+    cout << subtract(c, 2) << endl;
 
     return 0;
 }
